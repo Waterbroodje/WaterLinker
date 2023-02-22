1940c6d49cdd8f00d4526e06f5f66ae91d65622c
timokorpershoek <wortelschaaf.info@gmail.com>
2023-02-22 18:29:29 +0100

    Methods linkAccount and unlinkAccount not return true if the link is successfully completed

--- src/main/java/me/waterbroodje/waterlinker/api/DataGetter.java
+++ src/main/java/me/waterbroodje/waterlinker/api/DataGetter.java
@@ -6,6 +6,9 @@ import net.dv8tion.jda.api.entities.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
 import java.util.UUID;
 
 public class DataGetter {
@@ -17,7 +20,13 @@ public class DataGetter {
         plugin = waterLinker;
     }
 
-    public static Object getLinkInformation(LinkDataType linkDataType, UUID uuid) {
+    /**
+     * Get information from linked Player
+     * @param linkDataType data you want to retrieve
+     * @param uuid the uuid of the player
+     * @return Object you want to retrieve
+     */
+    public Object getLinkInformation(LinkDataType linkDataType, UUID uuid) {
         User user = plugin.getJda().getUserById(databaseExecution.getDiscordId(uuid));
 
         if (user != null) {
@@ -45,7 +54,70 @@ public class DataGetter {
         }
     }
 
-    public static Object getLinkInformation(LinkDataType linkDataType, Player player) {
-        return getLinkInformation(linkDataType, player.getUniqueId());
+    /**
+     * Get information from linked Player
+     * @param linkDataType data you want to retrieve
+     * @param player the player
+     * @return Object you want to retrieve
+     */
+    public Object getLinkInformation(LinkDataType linkDataType, Player player) {
+        return this.getLinkInformation(linkDataType, player.getUniqueId());
+    }
+
+    /**
+     * checks if the player is linked
+     * @param uuid the uuid of the player
+     * @return true if the user is linked, false if the user isn't linked
+     */
+    public boolean isLinked(UUID uuid) {
+        return databaseExecution.isLinked(uuid);
+    }
+
+    /**
+     * checks if the player is linked
+     * @param player the player
+     * @return true if the user is linked, false if the user isn't linked
+     */
+    public boolean isLinked(Player player) {
+        return this.isLinked(player.getUniqueId());
+    }
+
+    /**
+     * Links a Minecraft account to a Discord account
+     * @param uuid the uuid of the player
+     * @param discordId the discord id of the user
+     * @return true if the player linked successfully, false if there is an error
+     */
+    public boolean linkAccount(UUID uuid, Long discordId) {
+        Date date = Date.from(Instant.now());
+        return databaseExecution.linkAccount(uuid, discordId.toString(), new Timestamp(date.getTime()));
+    }
+
+    /**
+     * Links a Minecraft account to a Discord account
+     * @param player the uuid of the player
+     * @param discordId the discord id of the user
+     * @return true if the player linked successfully, false if there is an error
+     */
+    public boolean linkAccount(Player player, Long discordId) {
+        return this.linkAccount(player.getUniqueId(), discordId);
+    }
+
+    /**
+     * Unlinks a Minecraft account frm a Discord account
+     * @param uuid the uuid of the player
+     * @return true if the player unlinked successfully, false if there is an error
+     */
+    public boolean unlinkAccount(UUID uuid) {
+        return databaseExecution.unlinkAccount(uuid);
+    }
+
+    /**
+     * Unlinks a Minecraft account frm a Discord account
+     * @param player the player
+     * @return true if the player unlinked successfully, false if there is an error
+     */
+    public boolean unlinkAccount(Player player) {
+        return unlinkAccount(player.getUniqueId());
     }
 }
