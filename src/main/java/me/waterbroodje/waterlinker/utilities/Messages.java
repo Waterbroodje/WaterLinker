628651f663949b8f0ce12d89ee45333b32554862
timokorpershoek <wortelschaaf.info@gmail.com>
2023-02-22 18:51:41 +0100

    Added more messages and added a method to get the message without color codes

--- src/main/java/me/waterbroodje/waterlinker/utilities/Messages.java
+++ src/main/java/me/waterbroodje/waterlinker/utilities/Messages.java
@@ -22,9 +22,11 @@ public class Messages {
 
     public enum Message {
         ERROR_ONLY_HUMANS("error.only-humans", "&cOnly humans can execute this command!"),
-        ERROR_ALREADY_LINKED("error.already-linked", "&cYour Minecraft account is already linked to a Discord account. Type /link info for more information such as the account you are linked to."),
+        ERROR_ALREADY_LINKED("error.already-linked", "&cYour Minecraft account is already linked to a Discord account. Type `/link info` for more information such as the account you are linked to."),
         ERROR_NOT_LINKED("error.not-linked", "&cYou are not linked!"),
+        ERROR_SOMETHING_WENT_WRONG("error.something-went-wrong", "&cSomething went wrong executing this command. Please contact an administrator to get more information about this."),
         SUCCESS_LINKED_SUCCESS("success.linked-success", "&aYou are now linked!"),
+        SUCCESS_UNLINK_SUCCESS("success.unlink-success", "&aYou are not linked anymore. If this was a mistake, you can link again through `/link`."),
         NORMAL_CODE("normal.code", "&aHere's your personal code: &7%code%&a. Please type `/link <code>` in our discord server, and your account will be linked. Remember not to share this code with anyone, not even your friends!");
 
         private final String path;
@@ -38,7 +40,7 @@ public class Messages {
         public String get(Player player) {
             String value = config.getString(path, defaultValue);
             if (value.contains("%")) {
-                return PlaceholderAPI.setPlaceholders(player, value);
+                return color(PlaceholderAPI.setPlaceholders(player, value));
             } else {
                 return color(value);
             }
@@ -47,7 +49,7 @@ public class Messages {
         public String get(String code) {
             String value = config.getString(path, defaultValue);
             if (value.contains("%")) {
-                return PlaceholderAPI.setPlaceholders(null, value).replace("%code%", code);
+                return color(PlaceholderAPI.setPlaceholders(null, value).replace("%code%", code));
             } else {
                 return color(value);
             }
@@ -56,10 +58,14 @@ public class Messages {
         public String get() {
             String value = config.getString(path, defaultValue);
             if (value.contains("%")) {
-                return PlaceholderAPI.setPlaceholders(null, value);
+                return color(PlaceholderAPI.setPlaceholders(null, value));
             } else {
                 return color(value);
             }
+        }
+
+        public String getBlanco() {
+            return ChatColor.stripColor(config.getString(path, defaultValue));
         }
     }
 }
